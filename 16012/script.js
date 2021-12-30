29a79ac67cd980dad8032138e4ef3e81c77d08cf
Igor Nowik <nowik.igor@gmail.com>
2021-12-30 21:35:51 +0100

    3,69

--- 16012/script.js
+++ 16012/script.js
@@ -13,27 +13,28 @@ $(document.body).ready(function () {
 
     poems = content.replace(/\r\n|\r/g, '\n').trim().split('\n\n\n\n');
 
-    if (poems && poems.length) {
-      $('#butt').on('click', function () {
-        while (randomNumber === lastRandomNumber) {
-          randomNumber = parseInt((Math.random() * poems.length));
-          if (poems.length === 1) { break; }
-        }
-        lastRandomNumber = randomNumber;
-        
-        poem = poems[randomNumber];
-        title = poem.split('\n\n')[0];
-        text = poem.split('\n\n').slice(start=1).join('\n\n');
-        
-        $('#title').text(title);
-        
-        if (text.split('\n').length <= 20) {
-          $('#text1').html(text); }
-        else {
-          $('#text1').html(poem).split('\n').slice(end=20).join('\n');
-          $('#text2').html(poem).split('\n').slice(start=21).join('\n'); }
-
-      });
+    function next() {
+      while (randomNumber === lastRandomNumber) {
+        randomNumber = parseInt((Math.random() * poems.length));
+        if (poems.length === 1) { break; }
+      }
+      lastRandomNumber = randomNumber;
+      
+      poem = poems[randomNumber];
+      title = poem.split('\n\n')[0];
+      text = poem.split('\n\n').slice(start=1).join('\n\n');
+      
+      $('#title').text(title);
+      
+      if (text.split('\n').length <= 20) {
+        $('#text1').html(text); }
+      else {
+        $('#text1').html(poem.split('\n').slice(end=20).join('\n'));
+        $('#text2').html(poem.split('\n').slice(start=21).join('\n')); }
     }
+
+    $('#butt').on('click', next);
+    $(document).ready(next);
+
   });
 });
